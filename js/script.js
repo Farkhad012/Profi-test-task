1906859dff87fa6f96554891a7b2f8ac1b743ba7
Farkhad012 <pharrellmusicpro@gmail.com>
2024-03-10 21:53:41 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ burgerMenuIcon.addEventListener("click", function (e) {
 });
 
 // Обработчик события для каждого элемента .menu__item
-menuItem.forEach(function(item) {
-  item.addEventListener("click", function() {
+menuItem.forEach(function (item) {
+  item.addEventListener("click", function () {
     headerMenu.classList.remove("open");
   });
 });
@@ -21,4 +21,36 @@ document.addEventListener("click", function (e) {
     headerMenu.classList.remove("open");
   }
 });
+
+// Выкатываем заголовок при загрузке страницы
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Сохраняем ссылки на элементы
+  var title = document.querySelector('.title');
+  var aboutSteps = document.querySelector('.about__steps');
+  var btnBlack = document.querySelector('.btn__black');
+  var menuHeader = document.querySelector('.menu__header');
+
+  // Отображаем анимированный заголовок и другие элементы после загрузки страницы
+  setTimeout(function () {
+    animateElement(title, 'left', '0');
+    animateElement(title, 'opacity', '1');
+
+    animateElement(aboutSteps, 'left', '0');
+    animateElement(aboutSteps, 'opacity', '1');
+
+    animateElement(btnBlack, 'opacity', '1');
+
+    animateElement(menuHeader, 'opacity', '1');
+    
+  }, 500);
+
+  // Функция для анимации элемента
+  function animateElement(element, property, value) {
+    element.style[property] = value;
+  }
+
+});
+  
+
 
